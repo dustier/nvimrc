037368d19d63d1cb86f70737158e4ed7915ed763
dustier <dustier@foxmail.com>
2021-12-03 21:25:35 +0800

    neogen: <C-e> to jump to next position

--- lua/gen.lua
+++ lua/gen.lua
@@ -3,4 +3,5 @@ require('neogen').setup{
 }
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-e>", ":lua require('neogen').jump_next()<CR>", { silent = true, noremap = true })
 
