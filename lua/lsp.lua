4937251ffb52e4f6a57fae73542c26dbddbea347
dustier <dustier@foxmail.com>
2021-12-07 14:56:29 +0800

    add rust lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -28,7 +28,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = {'clangd', 'pyright', 'gopls', 'cmake', 'bashls'}
+local servers = {'clangd', 'rust_analyzer', 'pyright', 'gopls', 'cmake', 'bashls'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup{
     on_attach = on_attach,
