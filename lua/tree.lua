d35a5484c0e1082d5dc4a3d677dec6587d4383b6
dustier <dustier@foxmail.com>
2022-08-17 17:38:59 +0800

    nvim-tree: auto close when nvim-tree is the last window

--- lua/tree.lua
+++ lua/tree.lua
@@ -37,3 +37,12 @@ require'nvim-tree'.setup {
         ignore = false
     }
 }
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
