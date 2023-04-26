21760b0bed5d6070e36fc65d1b18194648bdf79e
dustier <dustier@foxmail.com>
2023-04-26 22:19:20 +0800

    fix nvim-tree

--- lua/tree.lua
+++ lua/tree.lua
@@ -1,42 +1,43 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
--- default mappings
-local list = {
-  { key = {"l"},    cb = tree_cb("edit") },
-  { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
-  { key = "e",                        cb = tree_cb("vsplit") },
-  { key = "<",                            cb = tree_cb("prev_sibling") },
-  { key = ">",                            cb = tree_cb("next_sibling") },
-  { key = "h",                         cb = tree_cb("close_node") },
-  { key = "K",                            cb = tree_cb("first_sibling") },
-  { key = "J",                            cb = tree_cb("last_sibling") },
-  { key = "R",                            cb = tree_cb("refresh") },
-  { key = "a",                            cb = tree_cb("create") },
-  { key = "d",                            cb = tree_cb("remove") },
-  { key = "r",                            cb = tree_cb("rename") },
-  { key = "<C-r>",                        cb = tree_cb("full_rename") },
-  { key = "x",                            cb = tree_cb("cut") },
-  { key = "c",                            cb = tree_cb("copy") },
-  { key = "p",                            cb = tree_cb("paste") },
-  { key = "y",                            cb = tree_cb("copy_name") },
-  { key = "Y",                            cb = tree_cb("copy_path") },
-  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-  { key = "-",                            cb = tree_cb("dir_up") },
-  { key = "?",                           cb = tree_cb("toggle_help") },
-}
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
 
-require'nvim-tree'.setup {
-    reload_on_bufenter = true,
-    hijack_cursor = true,
-    view = {
-        mappings = {
-            custom_only = true,
-            list = list
-        }
-    },
-    git = {
-        ignore = false
-    }
-}
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', 'l',  api.node.open.edit,                    opts('Open'))
+  vim.keymap.set('n', '>',     api.node.navigate.sibling.next,        opts('Next Sibling'))
+  vim.keymap.set('n', '<',     api.node.navigate.sibling.prev,        opts('Previous Sibling'))
+  vim.keymap.set('n', 'h',  api.node.navigate.parent_close,        opts('Close Directory'))
+  -- vim.keymap.set('n', 'R',     api.tree.reload,                       opts('Refresh'))
+  vim.keymap.set('n', 'a',     api.fs.create,                         opts('Create'))
+  vim.keymap.set('n', 'd',     api.fs.remove,                         opts('Delete'))
+  vim.keymap.set('n', 'r',     api.fs.rename_basename,                opts('Rename: Basename'))
+  vim.keymap.set('n', 'R',     api.fs.rename,                         opts('Rename'))
+  vim.keymap.set('n', 'x',     api.fs.cut,                            opts('Cut'))
+  vim.keymap.set('n', 'c',     api.fs.copy.node,                      opts('Copy'))
+  vim.keymap.set('n', 'p',     api.fs.paste,                          opts('Paste'))
+  vim.keymap.set('n', 'y',     api.fs.copy.filename,                  opts('Copy Name'))
+  vim.keymap.set('n', 'Y',     api.fs.copy.relative_path,             opts('Copy Relative Path'))
+  vim.keymap.set('n', 'gy',    api.fs.copy.absolute_path,             opts('Copy Absolute Path'))
+  vim.keymap.set('n', 'e', api.node.open.vertical,                opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview,                 opts('Open Preview'))
+  vim.keymap.set('n', 'D',     api.fs.trash,                          opts('Trash'))
+  vim.keymap.set('n', 'F',     api.live_filter.clear,                 opts('Clean Filter'))
+  vim.keymap.set('n', 'f',     api.live_filter.start,                 opts('Filter'))
+  vim.keymap.set('n', 'm',     api.marks.toggle,                      opts('Toggle Bookmark'))
+  vim.keymap.set('n', '?',    api.tree.toggle_help,                  opts('Help'))
+  -- vim.keymap.set('n', 'q',     api.tree.close,                        opts('Close'))
+end
+
+require("nvim-tree").setup({
+  on_attach = on_attach,
+  reload_on_bufenter = true,
+  hijack_cursor = true,
+  git = {
+    ignore = false
+  }
+})
 
 vim.api.nvim_create_autocmd("BufEnter", {
   nested = true,
@@ -46,3 +47,4 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end
 })
+
